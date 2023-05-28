4dc39eddddcb5c52803d2b7628543b85ea81e4bc
Frederik <47461904+Freeder1k@users.noreply.github.com>
2023-05-28 13:59:56 +0200

    add template command

---
+++ commands/prefixed/templateCommand.py
@@ -0,0 +1,19 @@
+from discord import Permissions
+
+import config
+from commands import command, commandEvent
+
+
+class TemplateCommand(command.Command):
+    def __init__(self):
+        super().__init__(
+            name="",
+            aliases=(),
+            usage=f"{config.PREFIX}",
+            description="",
+            req_perms=Permissions().none(),
+            permission_lvl=command.PermissionLevel.ANYONE
+        )
+
+    async def _execute(self, event: commandEvent.CommandEvent):
+        pass
