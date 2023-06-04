49e385ceddcc5af379db07fa18f1d00c500690e1
Frederik <47461904+Freeder1k@users.noreply.github.com>
2023-06-04 19:43:56 +0200

    minor changes

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import discord
 from discord.ext import tasks
 from dotenv import load_dotenv
 
+import api.minecraft
 import api.nasa
 import api.rateLimit
 import api.wynncraft
 import api.wynncraft.network
 import api.wynncraft.wynnAPI
 import commands.commandListener
+import commands.prefixed.configCommand
 import commands.prefixed.helpCommand
 import commands.prefixed.lastseenCommand
 import commands.prefixed.playtimeCommand
 import commands.prefixed.spaceCommand
 import commands.prefixed.wandererCommand
-import commands.prefixed.configCommand
 import serverConfig
 import storage.manager
 import storage.playtimeData
 import storage.playtimeData
 import utils.logging
-import api.minecraft
 
 load_dotenv()
 import os
@@ -36,43 +36,41 @@ client = discord.Client(intents=intents)
 start_time = datetime.now(timezone.utc)
 
 stopped = threading.Event()
+initialized = False
 
 
 @client.event
 async def on_ready():
+    utils.logging.log("Ready")
     utils.logging.log(f"Logged in as {client.user}")
     utils.logging.log(f"Guilds: {[g.name for g in client.guilds]}")
 
-    await serverConfig.load_server_configs()
-    await storage.manager.init_database()
-    await api.wynncraft.wynnAPI.init_sessions()
-    await api.nasa.init_session()
-    await api.minecraft.init_session()
+    global initialized
+    if not initialized:
+        await serverConfig.load_server_configs()
+        await storage.manager.init_database()
+        await api.wynncraft.wynnAPI.init_sessions()
+        await api.nasa.init_session()
+        await api.minecraft.init_session()
 
-    start_scheduling()
+        start_scheduling()
 
-    commands.commandListener.on_ready(client)
+        commands.commandListener.on_ready(client)
 
-    commands.commandListener.register_commands(
-        commands.prefixed.helpCommand.HelpCommand(),
-        commands.prefixed.playtimeCommand.PlaytimeCommand(),
-        commands.prefixed.wandererCommand.WandererCommand(),
-        commands.prefixed.lastseenCommand.LastSeenCommand(),
-        commands.prefixed.spaceCommand.SpaceCommand(),
-        commands.prefixed.configCommand.ConfigCommand(),
-    )
+        commands.commandListener.register_commands(
+            commands.prefixed.helpCommand.HelpCommand(),
+            commands.prefixed.playtimeCommand.PlaytimeCommand(),
+            commands.prefixed.wandererCommand.WandererCommand(),
+            commands.prefixed.lastseenCommand.LastSeenCommand(),
+            commands.prefixed.spaceCommand.SpaceCommand(),
+            commands.prefixed.configCommand.ConfigCommand(),
+        )
 
-    today = datetime.now(timezone.utc).date()
-    if (await storage.playtimeData.get_first_date_after(today)) is None:
-        await storage.playtimeData.update_playtimes()
+        today = datetime.now(timezone.utc).date()
+        if (await storage.playtimeData.get_first_date_after(today)) is None:
+            await storage.playtimeData.update_playtimes()
 
-    await client.change_presence(
-        status=discord.Status.online,
-        activity=discord.Activity(
-            name=f"{await api.wynncraft.network.player_sum()} players play Wynncraft",
-            type=discord.ActivityType.watching,
-        ),
-    )
+        initialized = True
 
 
 @client.event
@@ -106,7 +104,15 @@ def stop_scheduling():
 def main():
     print("\n  *:･ﾟ✧(=^･ω･^=)*:･ﾟ✧\n")
 
-    client.run(os.getenv('BOT_TOKEN'))
+    async def runner():
+        async with client:
+            await client.start(os.getenv('BOT_TOKEN'))
+
+    try:
+        asyncio.run(runner())
+    except:
+        asyncio.run(stop())
+        return
 
 
 async def stop():
@@ -117,10 +123,8 @@ async def stop():
     await api.nasa.close()
     await api.minecraft.close()
     await storage.manager.close()
+    utils.logging.log("Stopped")
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    finally:
-        asyncio.run(stop())
+    main()
