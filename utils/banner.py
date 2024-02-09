2b4558ad93ae0aef77a0d35d1714eb98c18bfc3c
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-02-09 21:50:35 +0100

    crop banner images

---
+++ utils/banner.py
@@ -0,0 +1,19 @@
+
+colors = {
+    "WHITE": "FFFFFF",
+    "ORANGE": "D87F33",
+    "MAGENTA": "B24CD8",
+    "LIGHT_BLUE": "6699D8",
+    "YELLOW": "E5E533",
+    "LIME": "7FCC19",
+    "PINK": "F27FA5",
+    "GRAY": "4C4C4C",
+    "LIGHT_GRAY": "999999",
+    "CYAN": "4C7F99",
+    "PURPLE": "7F3FB2",
+    "BLUE": "334CB2",
+    "BROWN": "664C33",
+    "GREEN": "667F33",
+    "RED": "993333",
+    "BLACK": "191919"
+}
