c453023bbd35c89872840b27e8d35f2197101778
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-02-09 23:19:12 +0100

    implement banner image generation util

--- utils/banner.py
+++ utils/banner.py
@@ -1,19 +1,92 @@
+from PIL import Image, ImageOps
 
 colors = {
-    "WHITE": "FFFFFF",
-    "ORANGE": "D87F33",
-    "MAGENTA": "B24CD8",
-    "LIGHT_BLUE": "6699D8",
-    "YELLOW": "E5E533",
-    "LIME": "7FCC19",
-    "PINK": "F27FA5",
-    "GRAY": "4C4C4C",
-    "LIGHT_GRAY": "999999",
-    "CYAN": "4C7F99",
-    "PURPLE": "7F3FB2",
-    "BLUE": "334CB2",
-    "BROWN": "664C33",
-    "GREEN": "667F33",
-    "RED": "993333",
-    "BLACK": "191919"
+    "WHITE": "#FFFFFF",
+    "ORANGE": "#D87F33",
+    "MAGENTA": "#B24CD8",
+    "LIGHT_BLUE": "#6699D8",
+    "YELLOW": "#E5E533",
+    "LIME": "#7FCC19",
+    "PINK": "#F27FA5",
+    "GRAY": "#4C4C4C",
+    "LIGHT_GRAY": "#999999",
+    "CYAN": "#4C7F99",
+    "PURPLE": "#7F3FB2",
+    "BLUE": "#334CB2",
+    "BROWN": "#664C33",
+    "GREEN": "#667F33",
+    "RED": "#993333",
+    "BLACK": "#191919"
 }
+
+pattern_map = {
+    "BASE": "base",
+    "BORDER": "border",
+    "BRICKS": "bricks",
+    "CIRCLE": "circle",
+    "CREEPER": "creeper",
+    "CROSS": "cross",
+    "CURLY_BORDER": "curly_border",
+    "DIAGONAL_LEFT": "diagonal_left",
+    "DIAGONAL_RIGHT": "diagonal_right",
+    "DIAGONAL_UP_LEFT": "diagonal_up_left",
+    "DIAGONAL_UP_RIGHT": "diagonal_up_right",
+    "FLOWER": "flower",
+    "GLOBE": "globe",
+    "GRADIENT": "gradient",
+    "GRADIENT_UP": "gradient_up",
+    "HALF_HORIZONTAL": "half_horizontal",
+    "HALF_HORIZONTAL_BOTTOM": "half_horizontal_bottom",
+    "HALF_VERTICAL": "half_vertical",
+    "HALF_VERTICAL_RIGHT": "half_vertical_right",
+    "MOJANG": "mojang",
+    "PIGLIN": "piglin",
+    "RHOMBUS": "rhombus",
+    "SKULL": "skull",
+    "SMALL_STRIPES": "small_stripes",
+    "SQUARE_BOTTOM_LEFT": "square_bottom_left",
+    "SQUARE_BOTTOM_RIGHT": "square_bottom_right",
+    "SQUARE_TOP_LEFT": "square_top_left",
+    "SQUARE_TOP_RIGHT": "square_top_right",
+    "STRAIGHT_CROSS": "straight_cross",
+    "STRIPE_BOTTOM": "stripe_bottom",
+    "STRIPE_CENTER": "stripe_center",
+    "STRIPE_DOWNLEFT": "stripe_downleft",
+    "STRIPE_DOWNRIGHT": "stripe_downright",
+    "STRIPE_LEFT": "stripe_left",
+    "STRIPE_MIDDLE": "stripe_middle",
+    "STRIPE_RIGHT": "stripe_right",
+    "STRIPE_TOP": "stripe_top",
+    "STRIPE_SMALL": "small_stripes",  # Wynn api uses this
+    "TRIANGLE_BOTTOM": "triangle_bottom",
+    "TRIANGLE_TOP": "triangle_top",
+    "TRIANGLES_BOTTOM": "triangles_bottom",
+    "TRIANGLES_TOP": "triangles_top",
+}
+
+
+def _tint_image(src: Image.Image, color="#FFFFFF"):
+    alpha = None
+    if src.mode == "LA":
+        alpha = src.split()[-1]
+    src = src.convert("L")
+    result = ImageOps.colorize(src, (0, 0, 0, 0), color)
+    if alpha:
+        result.putalpha(alpha)
+    return result
+
+
+def create_banner(patterns: list[tuple[str, str]]) -> Image:
+    """
+    Create a banner with the given patterns.
+
+    :param patterns: A list of tuples in the format (color, pattern) to be used in the banner.
+    """
+
+    banner = Image.new("RGBA", (20, 40), colors["WHITE"])
+    for color, pattern in patterns:
+        im = Image.open(f"../assets/banner/{pattern_map[pattern]}.png")
+        im = _tint_image(im, colors[color])
+        banner = Image.alpha_composite(banner, im)
+
+    return banner
