14d4b4ae03a279c768ad435c98d357ad5e801135
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-11-19 19:24:25 +0100

    refactor botInstance.py

--- common/botInstance.py
+++ common/botInstance.py
@@ -7,72 +7,87 @@ import common.logging
 import workers.guildUpdater
 import workers.presenceUpdater
 from common.botConfig import BotConfig
-from common.commands.command import Command
+from common.commands import command
 from common.commands.commandListener import CommandListener
-from common.commands.hybridCommand import HybridCommand
 from common.guildLogger import GuildLogger
+from common.storage.serverConfigs import ServerConfigs
 
 
 class BotInstance(discord.Client):
     def __init__(self, bot_config: BotConfig):
+        """
+        Crate a new bot instance. This is the main class that handles the bot's functionality.
+
+        :param bot_config: The bot's configuration.
+        """
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
         super().__init__(intents=intents)
+
         self.bot_config = bot_config
+        self.server_configs = ServerConfigs(f'data/server_configs/{bot_config.BOT_NAME}.json')
+
         self._tree = app_commands.CommandTree(self)
         self._initialized = False
 
         self._commands = []
-        self._hybrid_commands = []
 
         self._command_listener = CommandListener(self)
 
         self._guild_logger = GuildLogger(self, bot_config)
 
-    def register_commands(self, *new_commands: Command):
+    def add_commands(self, *new_commands: command.Command):
         """
         Registers new commands to the command listener.
         """
         for cmd in new_commands:
-            if isinstance(cmd, HybridCommand):
-                self._hybrid_commands.append(cmd)
-            else:
-                self._commands.append(cmd)
+            self._commands.append(cmd)
 
         if self._initialized:
             self._command_listener.register_commands(*new_commands)
 
             for cmd in new_commands:
-                if isinstance(cmd, HybridCommand):
+                if isinstance(cmd, app_commands.Command):
                     self._tree.add_command(cmd)
 
-            await self._tree.sync()
+    async def sync_commands(self):
+        """
+        Syncs the command tree with the discord API.
+        """
+        await self._tree.sync()
+
+    async def _initialize(self):
+        common.logging.info("Initializing...")
+
+        await self.server_configs.load()
+
+        workers.presenceUpdater.add_client(self)
+        workers.guildUpdater.add_guild(self.bot_config.GUILD_NAME, self._guild_logger)
+
+        self._command_listener.register_commands(*self._commands)
+
+        common.logging.info("Syncing command tree...")
+        for cmd in self._commands:
+            if isinstance(cmd, discord.app_commands.Command):
+                self._tree.add_command(cmd)
+
+        await self._tree.sync()
+
+        self._initialized = True
 
     async def on_ready(self):
         try:
             common.logging.info(f"Logged in as {self.user}")
-            common.logging.info("Initializing...")
 
             if not self._initialized:
-                workers.presenceUpdater.add_client(self)
-                workers.guildUpdater.add_guild(self.bot_config.GUILD_NAME, self._guild_logger)
+                await self._initialize()
 
-                self._command_listener.register_commands(*self._commands, *self._hybrid_commands)
-
-                self._initialized = True
-
-                common.logging.info("Syncing command tree...")
-                for cmd in self._hybrid_commands:
-                    self._tree.add_command(cmd)
-
-                await self._tree.sync()
-
-                common.logging.info("Ready")
-                common.logging.info(f"Guilds: {[g.name for g in self.guilds]}")
+            common.logging.info("Ready")
+            common.logging.info(f"Guilds: {[g.name for g in self.guilds]}")
         except Exception as e:
             await common.logging.error(exc_info=e)
             raise e
 
     async def on_message(self, message: discord.Message):
-        asyncio.create_task(self._command_listener.on_message(message))
+        await asyncio.create_task(self._command_listener.on_message(message))
