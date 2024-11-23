7630991b1c842e1c1272a80f7dbbd1e14ca5faab
Frederik <47461904+Freeder1k@users.noreply.github.com>
2024-11-23 12:42:47 +0100

    refactor command.py

--- common/commands/command.py
+++ common/commands/command.py
@@ -2,12 +2,11 @@ from abc import ABC, abstractmethod
 from enum import IntEnum
 from typing import Collection
 
-from discord import Permissions, Member, TextChannel, Embed
+from discord import Permissions, Member, Embed
 
 import common.utils.discord
+from common.botInstance import BotInstance
 from common.commands.commandEvent import CommandEvent
-from common import botConfig
-from common.storage import serverConfig
 
 
 class PermissionLevel(IntEnum):
@@ -23,6 +22,7 @@ class Command(ABC):
     Base class for all commands. This class should be inherited and the _execute method should be overridden to
     create a new command. The run method should not be overridden.
     """
+
     def __init__(self, name: str, aliases: Collection[str], usage: str, description: str, req_perms: Permissions,
                  permission_lvl: PermissionLevel):
         self.name = name.lower()
@@ -33,84 +33,78 @@ class Command(ABC):
         self.permission_lvl = permission_lvl
 
     @abstractmethod
-    async def _execute(self, command_event: CommandEvent):
+    async def _execute(self, event: CommandEvent):
         """
         This method should be overridden to create a new command. This method is called when the command is run.
         """
         pass
 
-    async def run(self, command_event: CommandEvent):
+    async def run(self, event: CommandEvent):
         """
         Runs the command and checks if the user has the required permissions to run the command.
         """
-        if not command_event.channel.permissions_for(command_event.guild.me).send_messages:
+        if not event.channel.permissions_for(event.guild.me).send_messages:
             return
 
-        if not command_event.channel.permissions_for(command_event.guild.me).embed_links:
-            await command_event.channel.send("Please give me the Embed Links permission to run commands.")
+        if not event.channel.permissions_for(event.guild.me).embed_links:
+            await event.reply("Please give me the Embed Links permission to run commands.")
             return
 
-        if not self._allowed_user(command_event.sender):
-            await common.utils.discord.send_error(command_event.channel,
-                                           f"This command is only available for {self.permission_lvl.name}s.")
+        if not self._allowed_user(event.sender, event.bot):
+            await event.reply_error(f"This command is only available for {self.permission_lvl.name}s.")
             return
 
-        m_perms = common.utils.discord.get_missing_perms(command_event.channel, self.req_perms)
+        m_perms = common.utils.discord.get_missing_perms(event.channel, self.req_perms)
         if m_perms != Permissions.none():
-            embed = Embed(
-                color=botConfig.ERROR_COLOR,
+            embed = Embed(color=event.bot.config.ERROR_COLOR,
                 title="**To use this command please give me the following permission(s):**",
-                description=[p for p in Permissions.VALID_FLAGS if getattr(m_perms, p)]
-            )
-            await command_event.channel.send(embed=embed)
+                description=[p for p in Permissions.VALID_FLAGS if getattr(m_perms, p)])
+            await event.reply(embed=embed)
             return
 
-        await self._execute(command_event)
+        await self._execute(event)
 
-    async def send_help(self, channel: TextChannel):
+    async def man(self, event: CommandEvent):
         """
         Sends an embed with information about the command to the specified channel.
         """
-        help_embed = Embed(
-            color=botConfig.DEFAULT_COLOR,
-            title=f"**{self.name.capitalize()} Command Info:**"
-        )
-        help_embed.add_field(name="**Usage:**", value=f"``{self.usage}``", inline=False)
+        embed = Embed(color=event.bot.config.DEFAULT_COLOR, title=f"**{self.name.capitalize()} Command Info:**")
+        embed.add_field(name="**Usage:**", value=f"``{self.usage}``", inline=False)
         if len(self.aliases) > 0:
-            help_embed.add_field(name="**Aliases:**", value=', '.join(self.aliases), inline=False)
-        help_embed.add_field(name="**Permission level:**", value=str(self.permission_lvl.name), inline=False)
-        help_embed.add_field(name="**Description:**", value=self.description, inline=False)
+            embed.add_field(name="**Aliases:**", value=', '.join(self.aliases), inline=False)
+        embed.add_field(name="**Permission level:**", value=str(self.permission_lvl.name), inline=False)
+        embed.add_field(name="**Description:**", value=self.description, inline=False)
         if self.req_perms != Permissions.none():
-            help_embed.add_field(name="**Required permissions:**", value=str(self.req_perms), inline=False)
+            embed.add_field(name="**Required permissions:**", value=str(self.req_perms), inline=False)
 
-        await channel.send(embed=help_embed)
+        await event.reply(embed=embed)
 
-    def _allowed_user(self, member: Member) -> bool:
-        if member.id in botConfig.DEV_USER_IDS:
+    def _allowed_user(self, member: Member, bot: BotInstance) -> bool:
+        if member.id in bot.config.DEV_USER_IDS:
             return True
 
         if self.permission_lvl == PermissionLevel.ANYONE:
             return True
 
         if self.permission_lvl == PermissionLevel.MEMBER:
-            member_role = serverConfig.get_member_role_id(member.guild.id)
+            member_role = bot.server_configs.get(member.guild.id).member_role_id
             if member.get_role(member_role) is not None:
                 return True
 
         if self.permission_lvl <= PermissionLevel.STRAT:
-            strat_role = serverConfig.get_strat_role_id(member.guild.id)
+            strat_role = bot.server_configs.get(member.guild.id).strat_role_id
             if member.get_role(strat_role) is not None:
                 return True
 
         if self.permission_lvl <= PermissionLevel.CHIEF:
             if member.guild_permissions.administrator:
                 return True
-            chief_role = serverConfig.get_chief_role_id(member.guild.id)
+
+            chief_role = bot.server_configs.get(member.guild.id).chief_role_id
             if member.get_role(chief_role) is not None:
                 return True
 
-        if self.permission_lvl == PermissionLevel.DEV:
-            return False
+        return False
 
     def is_this_command(self, command: str) -> bool:
         """
