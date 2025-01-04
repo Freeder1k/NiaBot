25f089ac303d04902d8b500afed90247796a259a
Frederik <47461904+Freeder1k@users.noreply.github.com>
2025-01-04 16:02:41 +0100

    change act command to use stored data

--- common/commands/prefixed/activityCommand.py
+++ common/commands/prefixed/activityCommand.py
@@ -1,9 +1,11 @@
+import time
 from datetime import timedelta, datetime, timezone
 
 from async_lru import alru_cache
 from discord import Permissions, Embed
 
 import common.api.wynncraft.v3.guild
+import common.storage.playerTrackerData
 import common.storage.playtimeData
 import common.utils.discord
 from common.commands import command
@@ -11,42 +13,35 @@ from common.commands.commandEvent import PrefixedCommandEvent
 from common.types.wynncraft import GuildStats
 
 
-async def _get_playtime(uuid: str, _):
-    uuid = uuid.replace(" - ", "")
-    today = datetime.now(timezone.utc).date()
-    last_week = today - timedelta(days=7)
-
-    d1 = await common.storage.playtimeData.get_first_date_after_from_uuid(today, uuid)
-    d2 = await common.storage.playtimeData.get_first_date_after_from_uuid(last_week, uuid)
-    if d1 is None or d2 is None:
-        return '0 min'
-
-    pt1 = await common.storage.playtimeData.get_playtime(uuid, d1)
-    pt2 = await common.storage.playtimeData.get_playtime(uuid, d2)
-
-    if pt1 is None or pt2 is None:
-        return '0 min'
-    else:
-        return f'{(pt1.playtime - pt2.playtime)} min'
-
-
 @alru_cache(ttl=600)
-async def _create_activity_embed(bot_config):
+async def _create_activity_embed(bot_config, weeks):
     guild: GuildStats = await common.api.wynncraft.v3.guild.stats(name=bot_config.GUILD_NAME)
+
+    after_date = datetime.now(timezone.utc).date() - timedelta(days=7*weeks)
+
+    t1 = time.time()
+    playtime_data = await common.storage.playerTrackerData.get_playtimes_for_guild(guild.name, after_date)
+    t2 = time.time()
+
+    async def get_playtime(uuid, _):
+        uuid = uuid.lower().replace("-", "")
+        if uuid not in playtime_data:
+            return '0.0 min'
+        return f"{playtime_data[uuid]:.1f} min"
 
     embed = Embed(
         color=bot_config.DEFAULT_COLOR,
-        title=f"**Weekly Playtimes in {bot_config.GUILD_NAME}**",
+        title=f"**Playtimes in {bot_config.GUILD_NAME}** in the last {weeks} week(s)",
         description='⎯' * 32,
         timestamp=datetime.now(timezone.utc).replace(hour=0, minute=0, second=0)
     )
-    embed.set_footer(text="Last update")
+    embed.set_footer(text=f"Query took {t2 - t1:.2f}s.")
 
     await common.utils.discord.add_guild_member_tables(
         base_embed=embed,
         guild=guild,
-        data_function=_get_playtime,
-        sort_function=lambda t: int(t[:-4]),
+        data_function=get_playtime,
+        sort_function=lambda t: float(t[:-4]),
         sort_reverse=True
     )
 
@@ -58,13 +53,17 @@ class ActivityCommand(command.Command):
         super().__init__(
             name="activity",
             aliases=("act",),
-            usage=f"activity",
-            description=f"Get the playtime of all members in the guild for the past week.",
+            usage=f"activity [weeks]",
+            description=f"Get the playtime of all members in the guild for the past specified amount of weeks.",
             req_perms=Permissions().none(),
             permission_lvl=command.PermissionLevel.STRAT
         )
 
     async def _execute(self, event: PrefixedCommandEvent):
         async with event.waiting():
-            embed = await _create_activity_embed(event.bot.config)
+            if len(event.args) >= 2:
+                weeks = int(event.args[1])
+            else:
+                weeks = 1
+            embed = await _create_activity_embed(event.bot.config, weeks)
             await event.reply(embed=embed)
