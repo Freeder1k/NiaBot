5f3e0a9390751b5cd7a4da956813a339ec1c6d5a
Frederik <47461904+Freeder1k@users.noreply.github.com>
2025-03-21 18:15:32 +0100

    revert to using standard workerqueue, some other refactorings

--- workers/statTracker.py
+++ workers/statTracker.py
@@ -15,140 +15,39 @@ import common.storage.usernameData
 import workers.usernameUpdater
 from common.types.dataTypes import MinecraftPlayer
 from common.types.enums import PlayerIdentifier
-
-
-class PlayerQueueWorker:
-    def __init__(self, delay=0.01):
-        """
-        QueueWorker for player stats.
-        """
-        self._queue = asyncio.Queue()
-        self._error_count = 0
-        self._delay = delay
-        self._task: asyncio.Task = None
-        self._delayed_tasks = set()
-
-    async def _record_stats(self, uuid: str, tries: int):
-        if common.api.wynncraft.v3.session.calculate_remaining_requests() < 10:
-            wait_time = common.api.wynncraft.v3.session.ratelimit_reset_time()
-            await asyncio.sleep(wait_time + 1)
-
-        stats = None
-        try:
-            stats = await common.api.wynncraft.v3.player.stats(uuid=uuid)
-            player = MinecraftPlayer(uuid=stats.uuid, name=stats.username)
-            await workers.usernameUpdater.update_username(player)
-            await common.storage.playerTrackerData.add_record(stats)
-        except common.api.wynncraft.v3.player.UnknownPlayerException:
-            common.logging.debug(f"Couldn't get stats of player with uuid {uuid}: Unknown player.")
-        except aiohttp.client_exceptions.ClientResponseError as e:
-            if e.status == 500:
-                common.logging.warning(f"Error 500 for player with uuid: {uuid}")
-            elif tries < 1:
-                self.put_delayed(delay=30, uuid=uuid, tries=tries + 1)
-            else:
-                common.logging.error(f"Client error in player stat tracker: uuid: {uuid}")
-                raise e
-        except Exception as e:
-            common.logging.error(f"Exception in player stat tracker: uuid: {uuid}, stats: {stats}")
-            raise e
-
-    async def _worker(self):
-        while True:
-            try:
-                await asyncio.sleep((2 ** self._error_count) - 1)
-
-                # wait if we're close to the ratelimit
-                if common.api.wynncraft.v3.session.calculate_remaining_requests() < 10:
-                    wait_time = common.api.wynncraft.v3.session.ratelimit_reset_time()
-                    await asyncio.sleep(wait_time + 1)
-
-                uuid, tries = await self._queue.get()
-
-                try:
-                    await self._record_stats(uuid, tries)
-                finally:
-                    self._queue.task_done()
-
-                if self._error_count > 0:
-                    self._error_count -= 1
-
-                await asyncio.sleep(self._delay)
-            except (KeyboardInterrupt, SystemExit, asyncio.CancelledError) as e:
-                raise e
-            except Exception as ex:
-                common.logging.error(exc_info=ex)
-                if self._error_count < 12:
-                    self._error_count += 1
-
-    def put(self, uuid, tries=0):
-        """
-        Add a player to the queue for stat tracking.
-        """
-        self._queue.put_nowait((uuid, tries))
-
-    async def _put_delayed(self, delay, uuid, tries=0):
-        await asyncio.sleep(delay)
-        self.put(uuid, tries)
-
-    def put_delayed(self, delay, uuid, tries=0):
-        """
-        Add a player to the queue for stat tracking after a delay.
-        """
-        task = asyncio.create_task(self._put_delayed(delay, uuid, tries))
-
-        # Keep a reference to the task to prevent it from being garbage collected
-        self._delayed_tasks.add(task)
-        task.add_done_callback(self._delayed_tasks.discard)
-
-    async def join(self):
-        """
-        Block until all players in the queue have been processed.
-        """
-        await self._queue.join()
-
-    def qsize(self):
-        """
-        Number of players in the queue.
-        """
-        return self._queue.qsize()
-
-    def empty(self):
-        """
-        Return True if the queue is empty, False otherwise.
-        """
-        return self._queue.empty()
-
-    @property
-    def started(self):
-        """
-        Return True if the worker is running, False otherwise.
-        """
-        return self._task is not None
-
-    def start(self):
-        """
-        Start the worker.
-        """
-        if self._task is not None:
-            raise RuntimeError("Worker already running")
-        self._task = asyncio.create_task(self._worker())
-
-    def stop(self):
-        """
-        Stop the worker.
-        """
-        if self._task is not None:
-            self._task.cancel()
-        self._error_count = 0
-        self._task = None
-
+from workers.queueWorker import QueueWorker
 
 _online_players: set[str] = set()
-_worker = PlayerQueueWorker()
+_worker = QueueWorker()
 
 
-@tasks.loop(seconds=61, reconnect=True)
+async def _record_stats(uuid: str, tries: int):
+    if common.api.wynncraft.v3.session.calculate_remaining_requests() < 10:
+        wait_time = common.api.wynncraft.v3.session.ratelimit_reset_time()
+        await asyncio.sleep(wait_time + 1)
+
+    stats = None
+    try:
+        stats = await common.api.wynncraft.v3.player.stats(uuid=uuid)
+        player = MinecraftPlayer(uuid=stats.uuid, name=stats.username)
+        await workers.usernameUpdater.update_username(player)
+        await common.storage.playerTrackerData.add_record(stats)
+    except common.api.wynncraft.v3.player.UnknownPlayerException:
+        common.logging.debug(f"Couldn't get stats of player with uuid {uuid}: Unknown player.")
+    except aiohttp.client_exceptions.ClientResponseError as e:
+        if e.status == 500:
+            common.logging.warning(f"Error 500 for player with uuid: {uuid}")
+        elif tries < 1:
+            _worker.put_delayed(_record_stats, 30, uuid, tries + 1)
+        else:
+            common.logging.error(f"Client error in player stat tracker: uuid: {uuid}")
+            raise e
+    except Exception as e:
+        common.logging.error(f"Exception in player stat tracker: uuid: {uuid}, stats: {stats}")
+        raise e
+
+
+@tasks.loop(seconds=30, reconnect=True)
 async def _update_online():
     try:
         global _online_players
@@ -159,13 +58,15 @@ async def _update_online():
         joined_players = _online_players - prev_online_players
         left_players = prev_online_players - _online_players
 
+        if len(prev_online_players) == 0:
+            return
         for uuid in joined_players:
-            _worker.put(uuid)
+            _worker.put(_record_stats, uuid, 0)
         for uuid in left_players:
-            _worker.put(uuid)
+            _worker.put(_record_stats, uuid, 0)
 
         if _worker.qsize() >= 100:
-            common.logging.debug(f"Tracking {len(left_players)}({_worker.qsize()}) player's stats.")
+            common.logging.debug(f"Tracking {len(joined_players) + len(left_players)}({_worker.qsize()}) player's stats.")
     except common.api.rateLimit.RateLimitException:
         pass
     except Exception as ex:
--- workers/usernameUpdater.py
+++ workers/usernameUpdater.py
@@ -1,8 +1,4 @@
-import asyncio
 from abc import ABC, abstractmethod
-
-import aiohttp.client_exceptions
-from discord.ext import tasks
 
 import common.api.minecraft
 import common.api.rateLimit
@@ -13,8 +9,6 @@ import common.logging
 import common.storage.playerTrackerData
 import common.storage.usernameData
 from common.types.dataTypes import MinecraftPlayer
-from common.types.enums import PlayerIdentifier
-from common.utils import minecraftPlayer
 from workers.queueWorker import QueueWorker
 
 _online_players: set[str] = set()
@@ -44,95 +38,3 @@ async def update_username(player: MinecraftPlayer):
     if prev_p is not None and prev_p.name != player.name:
         for subscriber in _subscribers:
             await subscriber.name_changed(player.uuid, prev_p.name, player.name)
-
-
-async def _fetch_and_update_username_mojang(username: str, tries: int = 1):
-    if common.api.minecraft.calculate_remaining_calls() < 1:
-        _worker.put_delayed(_fetch_and_update_username_mojang, 61, username, tries)
-        return
-    try:
-        player = await common.api.minecraft.get_player(username=username, use_mojang=True)
-    except common.api.rateLimit.RateLimitException as e:
-        common.logging.error("Rate limited by mojang api!", e)
-        _worker.put_delayed(_fetch_and_update_username_mojang, 61, username, tries)
-        return
-    except aiohttp.client_exceptions.ClientError as e:
-        common.logging.error(f"Failed to update username ({tries}/3): ", username)
-        if tries < 3:
-            _worker.put_delayed(_fetch_and_update_username_mojang, 61, username, tries + 1)
-        raise e
-
-    if player is None:
-        common.logging.debug(f"{username} is not a minecraft name but online on wynncraft ({tries}/3)!")
-        if tries < 3:
-            _worker.put_delayed(_fetch_and_update_username_mojang, 300, username, tries + 1)
-        return
-
-    _queued_names.discard(username)
-    # Fix for api having outdated usernames
-    player = minecraftPlayer.MinecraftPlayer(uuid=player.uuid, name=username)
-    await update_username(player)
-
-
-async def _fetch_and_update_username(username: str, tries: int = 1):
-    try:
-        player = await common.api.minecraft.get_player(username=username)
-    except common.api.rateLimit.RateLimitException as e:
-        common.logging.error("Rate limited!", e)
-        await asyncio.sleep(60)
-        _worker.put(_fetch_and_update_username, username)
-        return
-    except aiohttp.client_exceptions.ClientError as e:
-        common.logging.error(f"Failed to update username ({tries}/3): ", username)
-        if tries < 3:
-            _worker.put_delayed(_fetch_and_update_username, 60, username, tries + 1)
-        raise e
-
-    if player is None:
-        await _fetch_and_update_username_mojang(username, tries)
-        return
-
-    _queued_names.discard(username)
-    await update_username(player)
-
-
-@tasks.loop(seconds=61, reconnect=True)
-async def _update_usernames():
-    try:
-        global _online_players
-        prev_online_players = _online_players
-        _online_players = set((await common.api.wynncraft.v3.player.player_list(PlayerIdentifier.USERNAME)).keys())
-
-        joined_players = _online_players - prev_online_players
-
-        known_names = {p.name for p in await common.storage.usernameData.get_players(usernames=list(joined_players))}
-        unknown_names = [name for name in joined_players if name not in known_names]
-
-        for username in unknown_names:
-            if username not in _queued_names:
-                _worker.put(_fetch_and_update_username, username)
-
-        if _worker.qsize() >= 20:
-            common.logging.debug(f"Updating {len(unknown_names)}({_worker.qsize()}) minecraft usernames.")
-
-    except common.api.rateLimit.RateLimitException:
-        pass
-    except Exception as ex:
-        await common.logging.error(exc_info=ex)
-        raise ex
-
-
-_update_usernames.add_exception_type(aiohttp.client_exceptions.ClientError, Exception)
-
-
-def start():
-    # _update_usernames.start()
-    # _worker.start()
-    # common.logging.info("Username updater worker started.")
-    pass
-
-
-def stop():
-    # _worker.stop()
-    # _update_usernames.stop()
-    pass
